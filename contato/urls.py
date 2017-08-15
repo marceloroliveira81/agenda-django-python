c079fe5f47f1938b763e30239bc62251a55fc90c
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-08-15 17:51:33 -0500

    Alteração contato\urls.py
    Add
    
    url - novo

--- contato/urls.py
+++ contato/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.detalhes, name='detalhes'),
+    url(r'^novo/$', views.novo, name='novo'),
 ]
