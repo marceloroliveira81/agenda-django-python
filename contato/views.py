218384e6324c8587d5134b5d2ddc5b48c24f7bc6
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-08-15 17:52:33 -0500

    Alteração contato\views.py
    Add
    
    import ContatoForm
    
    def novo

--- contato/views.py
+++ contato/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .models import Contato
 
+from .forms import ContatoForm
+
 # Create your views here.
 
 def index(request):
@@ -11,3 +13,12 @@ def index(request):
 def detalhes(request, pk):
 	contato = Contato.objects.get(pk=pk)
 	return render(request, 'contato/detalhes.html', {'contato' : contato})
+
+def novo(request):
+	#if request.method == 'POST':
+	#	if form.is_valid():
+	
+	#else: 
+	form = ContatoForm()
+	
+	return render(request, 'contato/novo.html', {'form' : form})
