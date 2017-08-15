145f3458726a72e1ab569a7d3c210e1eb6abfcda
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-08-15 17:49:46 -0500

    Alteração contato\admin.py
    Add
    
    import AdminSite
    
    class ContatoAdmin
    registrado Contato e ContatoAdmin
    
    class CustonAdminSite
    registrado Contato e ContatoAdmin

--- contato/admin.py
+++ contato/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
+from django.contrib.admin import AdminSite
+
 from .models import Contato
 
 # Register your models here.
 
-admin.site.register(Contato)
+class ContatoAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'data_nascimento', 'email', 'sexo', 'estado_civil')
+	list_filter = ('sexo', 'estado_civil')
+	search_fields = ['nome']
+
+admin.site.register(Contato, ContatoAdmin)
+
+class CustonAdminSite(AdminSite):
+	site_header = 'Agenda - Contatos'
+	site_title = 'Agenda - Contatos'
+	index_title = 'Administração'
+
+admin_site = CustonAdminSite(name='myadmin')
+admin_site.register(Contato, ContatoAdmin)
