af27d6f009fa586ed56ddf56830b2848733179c2
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-08-20 22:00:49 -0500

    Alteração contato\urls.py
    Expressão para alterar e excluir

--- contato/urls.py
+++ contato/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.detalhes, name='detalhes'),
     url(r'^novo/$', views.novo, name='novo'),
+    url(r'^alterar/(?P<pk>[0-9]+)/$', views.alterar, name='alterar'),
+    url(r'^excluir/(?P<pk>[0-9]+)/$', views.excluir, name='excluir'),
 ]
