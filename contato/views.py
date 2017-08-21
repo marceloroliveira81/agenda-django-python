51dbcddee6fad7e09a064932320cf4b2a250dea7
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-08-20 22:04:15 -0500

    Alteração contato\views.py
    import redirect e get_object_or_404
    
    inclusão da rotina para salvar (def novo)
    
    inclusão da rotina para alterar (def alterar)
    
    inclusão da rotina para excluir (def excluir)

--- contato/views.py
+++ contato/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Contato
 
@@ -15,10 +15,33 @@ def detalhes(request, pk):
 	return render(request, 'contato/detalhes.html', {'contato' : contato})
 
 def novo(request):
-	#if request.method == 'POST':
-	#	if form.is_valid():
-	
-	#else: 
-	form = ContatoForm()
+	if request.method == 'POST':
+		form = ContatoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('index')
+
+	else: 
+		form = ContatoForm()
 	
 	return render(request, 'contato/novo.html', {'form' : form})
+
+def alterar(request, pk):
+	contato = get_object_or_404(Contato, pk=pk)
+	if request.method == 'POST':
+		form = ContatoForm(request.POST, instance=contato)
+		if form.is_valid():
+			form.save()
+			return redirect('index')
+	
+	else: 
+		form = ContatoForm(instance=contato)
+	
+	return render(request, 'contato/novo.html', {'form' : form})
+
+def excluir(request, pk):
+	contato = get_object_or_404(Contato, pk=pk)
+	contato.delete()
+	return redirect('index')
+	#contatos = Contato.objects.all()
+	#return render(request, 'contato/index.html', {'contatos' : contatos})
